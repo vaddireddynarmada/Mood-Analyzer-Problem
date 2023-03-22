9b179e84c95b136e4b9a9f52931913f9902aba75
unknown <narmada.vaddireddy97@gmail.com>
2023-03-22 19:39:13 +0530

    Added: Code to analyse and respond Happy or Sad Mood

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
+    public void givenMessage_WhenSad_ShouldReturnSad()  {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad message");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
 
     @Test
-    public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
+    public void givenMessage_WhenHappy_ShouldReturnHappy(){
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Happy message");
         String mood = moodAnalyzer.analyzeMood();
         Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
